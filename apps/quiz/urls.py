ff9d51a145c153222e088a7c08d540ea61005395
Radek Czajka <radoslaw.czajka@nowoczesnapolska.org.pl>
2011-09-28 12:49:30 +0200

    urls fix

--- apps/quiz/urls.py
+++ apps/quiz/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('quiz.views',
     url(r'^$', 'question', name='quiz'),
-    url(r'^q/(?P<slug>[-a-z0-9]*)/$', 'question', name='quiz'),
-    url(r'^r/(?P<slug>[-a-z0-9]*)/$', 'result', name='quiz_result'),
+    url(r'^q/(?P<slug>[^/]*)/$', 'question', name='quiz'),
+    url(r'^r/(?P<slug>[^/]*)/$', 'result', name='quiz_result'),
 )
